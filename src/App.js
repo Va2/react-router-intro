ad436ba7704089a3d996f6548169e02993c4fa7f
Va2 <vadim.brieven@gmail.com>
2019-07-15 17:04:30 +0200

    add 404 error component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Nav from './components/Nav';
 import Home from './components/Home';
 import About from './components/About';
 import Shop from './components/Shop';
+import Error404 from './components/Error404';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/shop" component={Shop} />
+					<Route path="*" component={Error404} />
         </Switch>
       </div>
     </Router>
